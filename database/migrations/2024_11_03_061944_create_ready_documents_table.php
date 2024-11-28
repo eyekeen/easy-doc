c5a6f0476c44942a09a0567a821f7efc60989b61
tarum <block-code@mail.ru>
2024-11-28 10:08:33 +0300

    change migration

--- database/migrations/2024_11_03_061944_create_ready_documents_table.php
+++ database/migrations/2024_11_03_061944_create_ready_documents_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('ready_docoments', function (Blueprint $table) {
+        Schema::create('ready_documents', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('petition_id');
             $table->string('name');
@@ -26,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('ready_docoments');
+        Schema::dropIfExists('ready_documents');
     }
 };
